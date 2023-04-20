6ac17a7563620d504b318a181b6663b3a4038deb
Enrique Perez <eperezh@usm.cl>
2023-04-20 19:54:59 +0200

    segundo commit con comienzo del apartado 1

--- src/repository/usersRepository.js
+++ src/repository/usersRepository.js
@@ -2,14 +2,16 @@ import dataset from '../dataset/users_info.json'
 let usuarios = []
 usuarios = dataset
 
-
+export default function getUsersFromDataset () {
+    return dataset
+}
 
 
 exports.getUsersForNameInOrder = (nombre) => {
     // Se revisa si algun producto tiene la categoria
     let name_filter = usuarios.filter((user) => user.name.includes(nombre))
   
-    // En caso de no existir ningun producto retorna -1 (equivalente a que no fue encontrado) (Para captar el error 404)
+    // En caso de no existir ningun nombre retorna -1 (equivalente a que no fue encontrado) (Para captar el error 404)
     if (name_filter.length == 0 ) return -1
   
     // Se retornan los productos
